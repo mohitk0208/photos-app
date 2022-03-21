c1995ac2aa517efc480b08032031116fc4533baa
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-03-21 12:15:46 +0530

    customized tabs in tabscontainer

--- src/components/TabsContainer/TabsContainer.jsx
+++ src/components/TabsContainer/TabsContainer.jsx
@@ -4,30 +4,29 @@ import SearchPage from "../SearchPage"
 import SavedPage from "../SavedPage"
 import { HomeIcon, SearchIcon, BookmarkIcon } from "@heroicons/react/solid"
 
+
+const CustomTab = ({ Icon, name }) => {
+  return (
+    <Tab>
+      {({ selected }) => (
+        <div className={`group cursor-pointer`}>
+          <Icon className={`w-20 aspect-square ${selected ? "text-gray-700" : "text-gray-500"}  block px-2 py-0 group-hover:text-gray-600 transition-colors duration-150 ease-in-out`} />
+          <p className={`text-sm ${selected ? "text-gray-800" : "text-gray-700"}  group-hover:text-gray-800 transition-colors duration-150 ease-in-out`} >{name}</p>
+        </div>
+      )}
+    </Tab>
+  )
+}
+
 const TabsContainer = () => {
 
   return (
     <div>
       <Tab.Group as="div" >
         <Tab.List as="div" className=" w-full p-10 flex items-center justify-center gap-2 bg-gray-200 " >
-          <Tab>
-            <div>
-              <HomeIcon className="w-20 aspect-square text-gray-500 block p-2" />
-              <p>Home</p>
-            </div>
-          </Tab>
-          <Tab>
-            <div>
-              <SearchIcon className="w-20 aspect-square text-gray-500 block p-2" />
-              <p>Search</p>
-            </div>
-          </Tab>
-          <Tab>
-            <div>
-              <BookmarkIcon className="w-20 aspect-square text-gray-500 block p-2" />
-              <p>Saved</p>
-            </div>
-          </Tab>
+          <CustomTab Icon={HomeIcon} name="Home" />
+          <CustomTab Icon={SearchIcon} name="Search" />
+          <CustomTab Icon={BookmarkIcon} name="Saved" />
         </Tab.List>
 
         <Tab.Panels as="div" className="px-10 py-5 bg-gray-50 w-full h-full">

676604bed08e3c94df97af01de5f0fc353542386
Mohit Kumar <61026761+mohitk0208@users.noreply.github.com>
2022-03-24 22:51:41 +0530

    .

--- src/components/PhotosContainer/Photo.tsx
+++ src/components/PhotosContainer/Photo.tsx
@@ -26,7 +26,7 @@ const Photo = ({ photo, onClick }: PhotoProps) => {
   return (
     <div className="w-full aspect-square bg-gray-400 rounded-sm cursor-pointer overflow-hidden relative" onClick={onClick} >
       <img className="w-full h-full hover:scale-110 transition-transform duration-200 ease-in-out object-cover" src={photo.urls.regular} alt="" />
-      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/80" ></div>
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black/80 pointer-events-none" ></div>
 
       <BookmarkIcon className={`w-6 h-6 ${isPhotoSaved(photo.id) ? " fill-yellow-500 stroke-black" : "fill-transparent stroke-slate-50"} stroke-1  absolute bottom-1 right-1`} onClick={handleBookmarkClick} />
     </div>
